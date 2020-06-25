1e2bd2173467d0649e150eb14f6cf276aff4fa27
Pau Perez <saulopefa@gmail.com>
2020-06-25 17:17:24 +0200

    Line-through label when checkbox is checked
    This emphasizes when an item is complete.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './styles.css';
+
+var checkboxes = document.querySelectorAll("input[type='checkbox']");
+
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('change', () => {
+    const label = checkbox.labels[0]
+    label.classList.toggle('line-through');
+  });
+});
