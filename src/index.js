ad212eb0827e150ff05578f89b57ddb870cc5bdf
Pau Perez <saulopefa@gmail.com>
2020-06-25 19:15:04 +0200

    Persist checkbox's checked state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import './styles.css';
 
+function setState(checkbox) {
+  localStorage.setItem(checkbox.id, checkbox.checked);
+}
+
+function loadState(checkbox) {
+  return localStorage.getItem(checkbox.id) === 'true';
+}
+
 var checkboxes = document.querySelectorAll("input[type='checkbox']");
 
 checkboxes.forEach(checkbox => {
+  const label = checkbox.labels[0]
+
   checkbox.addEventListener('change', () => {
-    const label = checkbox.labels[0]
     label.classList.toggle('line-through');
+
+    setState(checkbox);
   });
+
+  checkbox.checked = loadState(checkbox);
+  if (checkbox.checked) {
+    label.classList.add('line-through');
+  }
 });
